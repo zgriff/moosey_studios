dfdc2742451ca4a520d26b7f1d217f17e11d1c79
Joshua Kaplan <jgk83@cornell.edu>
2021-03-23 11:50:29 -0400

    Started implementation of network data serialization. Interface should be complete

---
+++ Roshamboogie/source/Globals.h
@@ -0,0 +1,28 @@
+//
+//  Globals.h
+//  Roshamboogie
+//
+//  Created by Joshua Kaplan on 3/22/21.
+//  Copyright © 2021 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#ifndef Globals_h
+#define Globals_h
+
+namespace globals {
+
+/** Minimum number of players per game */
+constexpr uint8_t MIN_PLAYERS = 3;
+
+/** Maximum number of players per game */
+constexpr uint8_t MAX_PLAYERS = 8;
+
+/** Maximum number of orbs per game */
+constexpr uint8_t MAX_ORBS = 20;
+
+/** Maximum number of swap stations per game */
+constexpr uint8_t MAX_SWAP_STATIONS = 5;
+
+}
+
+#endif /* Globals_h */
