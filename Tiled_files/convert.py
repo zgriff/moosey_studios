4878c65661a10c07e7db195469ad331832b5fb22
Joshua Kaplan <jgk83@cornell.edu>
2021-04-03 17:10:45 -0400

    Boundary parsing

--- Tiled_files/convert.py
+++ Tiled_files/convert.py
@@ -21,6 +21,17 @@ out["egg_spawn_locations"] = [];
 out["orb_spawn_locations"] = [];
 out["player_spawn_locations"] = [];
 
+tiles = tmx.get_layer_by_name("Background")
+hb = tmx.get_layer_by_name("Hitboxes")
+dec = tmx.get_layer_by_name("Decorations")
+sp = tmx.get_layer_by_name("Spawners")
+
+# for x, y, image in tiles.tiles():
+#     pass
+for obj in hb:
+    boundary = [{"x": x, "y": y} for (x,y) in obj.as_points]
+    out["boundaries"].append(boundary)
+
 # write out filename.tmx to filename.json
 raw_name = os.path.splitext(os.path.basename(f))[0]
 out_path = os.path.join(os.path.dirname(f), "..", "..", "Roshamboogie", "assets", "maps", raw_name+".json")
