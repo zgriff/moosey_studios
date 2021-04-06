747d49ed092b73aaf0a8f5ea8dd7dccdcbcaee09
Joshua Kaplan <jgk83@cornell.edu>
2021-04-06 10:20:30 -0400

    Changed name of decoration tileset

--- Tiled_files/tilesets/decorations.tsx
+++ Tiled_files/tilesets/decorations.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="tree" tilewidth="33" tileheight="49" tilecount="1" columns="0">
+<tileset version="1.5" tiledversion="1.5.0" name="decorations" tilewidth="33" tileheight="49" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="33" height="49" source="../images/t.png"/>
