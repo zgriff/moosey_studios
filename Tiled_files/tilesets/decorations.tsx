27707cda49baa13cd6ac4fae475b91825ba8a994
Joshua Kaplan <jgk83@cornell.edu>
2021-04-09 14:18:21 -0400

    Fixed bug in map exporter

--- Tiled_files/tilesets/decorations.tsx
+++ Tiled_files/tilesets/decorations.tsx
@@ -1,7 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="decorations" tilewidth="33" tileheight="49" tilecount="1" columns="0">
+<tileset version="1.5" tiledversion="1.5.0" name="decorations" tilewidth="168" tileheight="321" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="33" height="49" source="../images/t.png"/>
+ <tile id="1">
+  <image width="118" height="190" source="../images/pine.png"/>
+ </tile>
+ <tile id="2">
+  <image width="153" height="321" source="../images/Tree 2 flat.png"/>
+ </tile>
+ <tile id="3">
+  <image width="168" height="258" source="../images/tree 3 flat.png"/>
  </tile>
 </tileset>
