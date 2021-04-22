f93319c90bed206fc0377a6fe6017701dd14aae1
Thomas Chen <tcc78@cornell.edu>
2021-04-22 00:13:29 -0400

    networking usernames

--- Tiled_files/tilesets/tiles.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="tiles" tilewidth="50" tileheight="50" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="2">
-  <image width="50" height="50" source="../images/grass-texture.png"/>
- </tile>
-</tileset>
